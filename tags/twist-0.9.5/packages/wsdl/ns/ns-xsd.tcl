66ab90800cc9445d18384ce261062e304c2a1685
tom.rmadilo <tom.rmadilo@45e038e7-871e-0410-ab0a-b1edb26ebeb7>
2007-05-27 00:44:16 +0000

    Update to allow the use of a complexType element name in place  of the procArgs and/or returnList in <ws>proc.
    Previously the API didn't allow user control over the name of the
     input or output element names. It is also verbose to require
     restating arguments and configurations for previously defined types.

---
+++ tags/twist-0.9.5/packages/wsdl/ns/ns-xsd.tcl
@@ -0,0 +1,104 @@
+# (c) 2006 United eWay
+# All rights reserved
+# Licensed under the New BSD license:
+# (http://www.opensource.org/licenses/bsd-license.php)
+# Contact: Tom Jackson <tom at junom.com>
+
+# Types for XML-Schema namespace="http://www.w3.org/2001/XMLSchema"
+
+# Create xsd namespace
+#namespace eval ::wsdb::types::xsd { }
+#namespace eval ::wsdb::schema::xsd { }
+#::wsdb::schema::appendAliasMap [list xsd "http://www.w3.org/2001/XMLSchema"]
+::wsdl::schema::new xsd "http://www.w3.org/2001/XMLSchema"
+
+# anySimpleType
+::wsdl::types::primitiveType::new xsd anySimpleType {return 1} {Base type, should return true for every case}
+::wsdl::doc::document doc types xsd anySimpleType {Base type, should return true for every case}
+
+# string
+::wsdl::types::primitiveType::new xsd string {return 1} {String type. Anything should pass as true}
+::wsdl::doc::document doc types xsd string {String type. Anything should pass as true}
+
+# dateTime
+::wsdl::types::primitiveType::new xsd dateTime "return \[::wsdb::types::tcl::dateTime::toArray \$value returnArray\]" "xml schema dateTime type"
+
+# duration
+::wsdl::types::primitiveType::new xsd duration "return \[::wsdb::types::tcl::dateTime::durationToArray \$value returnArray\]" "xml schema duration type"
+
+proc ::wsdb::types::xsd::duration::validate { duration } {
+
+    return [::wsdb::types::tcl::dateTime::durationToArray $duration returnArray]
+}
+
+
+proc ::wsdb::types::xsd::dateTime::validate { datetime } {
+    return [::wsdb::types::tcl::dateTime::toArray $datetime returnArray]
+}
+
+# boolean
+::wsdl::types::simpleType::restrictByEnumeration xsd boolean xsd::string {0 1 true false}
+
+# numeric types
+::wsdl::types::primitiveType::new xsd decimal "return \[string is double -strict \$value]" {faked up decimal type}
+::wsdl::types::primitiveType::new xsd float "return \[string is double -strict \$value]" {faked up float type}
+::wsdl::types::primitiveType::new xsd integer "return \[string is integer -strict \$value]" {integer type}
+
+namespace eval ::wsdb::types::xsd {
+
+    
+    variable minusOptional {(-)?}
+    variable minusOptionalAnchored {\A(-)?\Z}
+    
+    variable year {(-)?([0-9]{4}|[1-9]{1}[0-9]{4,})}
+    variable yearAnchored {\A(-)?([0-9]{4}|[1-9]{1}[0-9]{4,})\Z}
+    
+    variable timezone {(Z|(([\+\-]{1}))?((?:(14)(?::)(00))|(?:([0][0-9]|[1][0-3])(?::)([0-5][0-9]))))}
+    variable timezoneOptional ${timezone}?
+    variable timezoneAnchored "\\A$timezoneOptional\\Z"
+    
+    variable gYear ${year}${timezoneOptional}
+    variable gYearAnchored "\\A${gYear}\\Z"
+    
+    variable day {([0][0-9]|[12][0-9]|[3][01])}
+    
+    variable gDay ${day}${timezoneOptional}
+    variable gDayAnchored "\\A${gDay}\\Z"
+    
+    variable month {(?:([0][1-9]|[1][0-2]))}
+    
+    variable gMonth ${month}${timezoneOptional}
+    variable gMonthAnchored "\\A${gMonth}\\Z"
+    
+    variable gYearMonth ${year}(?:-)${month}
+    variable gYearMonthAnchored "\\A${gYearMonth}\\Z"
+    
+    variable gMonthDay ${month}(?:-)${day}
+    variable gMonthDayAnchored "\\A${gMonthDay}\\Z"
+    
+
+    ::wsdl::types::simpleType::restrictByPattern \
+	xsd minusOptional xsd::string $minusOptionalAnchored;
+    
+    ::wsdl::types::simpleType::restrictByPattern \
+	xsd year xsd::integer $yearAnchored
+    
+    ::wsdl::types::simpleType::restrictByPattern \
+	xsd timeZone xsd::string $timezoneAnchored
+    
+    ::wsdl::types::simpleType::restrictByPattern \
+	xsd gYear xsd::string $gYearAnchored
+    
+    ::wsdl::types::simpleType::restrictByPattern \
+	xsd gMonth  xsd::string $gMonthAnchored
+    
+    ::wsdl::types::simpleType::restrictByPattern \
+	xsd gDay  xsd::string $gDayAnchored
+    
+    ::wsdl::types::simpleType::restrictByPattern \
+	xsd gYearMonth  xsd::string $gYearMonthAnchored
+    
+    ::wsdl::types::simpleType::restrictByPattern \
+	xsd gMonthDay  xsd::string $gMonthDayAnchored
+
+}
